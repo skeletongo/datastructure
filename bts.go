4ec74827ff55c5ec06b15d66b1fdb53713422d9d
LiuZheng <liuzhengr@qq.com>
2019-03-31 01:36:44 +0800

    添加二分搜索测试文件

--- bts.go
+++ bts.go
@@ -108,22 +108,22 @@ func (b *Bst) Contains2(n int) bool {
 }
 
 // 前序遍历(递归)
-func (b *Bst) PreOrder() {
-	b.preOrder(b.node)
+func (b *Bst) PreOrder(f func(*Node)) {
+	b.preOrder(b.node, f)
 }
 
-func (b *Bst) preOrder(node *Node) {
+func (b *Bst) preOrder(node *Node, f func(*Node)) {
 	if node == nil {
 		return
 	}
 
-	fmt.Println(node.Num)
-	b.preOrder(node.Left)
-	b.preOrder(node.Right)
+	f(node)
+	b.preOrder(node.Left, f)
+	b.preOrder(node.Right, f)
 }
 
 // 前序遍历(非递归)
-func (b *Bst) PreOrder2() {
+func (b *Bst) PreOrder2(f func(*Node)) {
 	if b.node == nil {
 		return
 	}
@@ -135,7 +135,7 @@ func (b *Bst) PreOrder2() {
 		list = list[:top]
 		top--
 
-		fmt.Print(e.Num)
+		f(e)
 		if e.Right != nil {
 			list = append(list, e.Right)
 			top++
@@ -149,80 +149,95 @@ func (b *Bst) PreOrder2() {
 
 // *中序遍历又名深度优先遍历
 // 中序遍历(递归)
-func (b *Bst) InOrder() {
-	b.inOrder(b.node)
+func (b *Bst) InOrder(f func(*Node)) {
+	b.inOrder(b.node, f)
 }
 
-func (b *Bst) inOrder(node *Node) {
+func (b *Bst) inOrder(node *Node, f func(*Node)) {
 	if node == nil {
 		return
 	}
 
-	b.inOrder(node.Left)
-	fmt.Println(node.Num)
-	b.inOrder(node.Right)
+	b.inOrder(node.Left, f)
+	f(node)
+	b.inOrder(node.Right, f)
 }
 
 // 中序遍历(非递归)
-func (b *Bst) InOrder2() {
+func (b *Bst) InOrder2(f func(*Node)) {
 	if b.node == nil {
 		return
 	}
 
+	var s bool
 	top := 0
 	list := []*Node{b.node}
 	for len(list) > 0 {
 		e := list[top]
-		if e.Left != nil {
-			list = append(list, e.Left)
-			top++
-		} else {
+		if !s { // 压入
+			if e.Left != nil {
+				list = append(list, e.Left)
+				top++
+			} else {
+				list = list[:top]
+				top--
+				f(e)
+				s = true
+				if e.Right != nil {
+					list = append(list, e.Right)
+					top++
+					s = false
+				}
+			}
+		} else { // 弹出
 			list = list[:top]
 			top--
-
-			fmt.Println(e.Num)
+			f(e)
 			if e.Right != nil {
 				list = append(list, e.Right)
 				top++
+				s = false
 			}
 		}
 	}
 }
 
 // 后序遍历(递归)
-func (b *Bst) Postorder() {
-	b.postOrder(b.node)
+func (b *Bst) PostOrder(f func(*Node)) {
+	b.postOrder(b.node, f)
 }
 
-func (b *Bst) postOrder(node *Node) {
+func (b *Bst) postOrder(node *Node, f func(*Node)) {
 	if node == nil {
 		return
 	}
 
-	b.postOrder(node.Left)
-	b.postOrder(node.Right)
-	fmt.Println(node.Num)
+	b.postOrder(node.Left, f)
+	b.postOrder(node.Right, f)
+	f(node)
 }
 
 // 后序遍历(非递归)
-func (b *Bst) PostOrder2() {
+func (b *Bst) PostOrder2(f func(*Node)) {
 	if b.node == nil {
 		return
 	}
 
-	var cur *Node
+	var prev *Node
 	top := 0
 	list := []*Node{b.node}
 	for len(list) > 0 {
 		e := list[top]
-		if e.Right == cur { // 相同弹出
-			fmt.Println(e.Num)
+		if (e.Left == nil && e.Right == nil) || (prev != nil && (e.Right == prev || e.Left == prev)) {
 			list = list[:top]
 			top--
-			cur = e
-		} else { // 不同压入
-			list = append(list, e.Right)
-			top++
+			f(e)
+			prev = e
+		} else {
+			if e.Right != nil {
+				list = append(list, e.Right)
+				top++
+			}
 			if e.Left != nil {
 				list = append(list, e.Left)
 				top++
--- bts_test.go
+++ bts_test.go
@@ -1,1 +1,112 @@
 package dataStructure
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Same(b Bst, f func(bst Bst) []int, res []int) bool {
+	list := f(b)
+	if len(list) != len(res) {
+		return false
+	}
+	for k, v := range res {
+		if list[k] != v {
+			return false
+		}
+	}
+	return true
+}
+
+func Pt(list []int) {
+	for _, v := range list {
+		fmt.Println(v)
+	}
+}
+
+func TestBst_Add(t *testing.T) {
+	/////////////////
+	//       5     //
+	//     /   \   //
+	//    3     8  //
+	//   / \   /   //
+	//  2   4 6    //
+	// /       \   //
+	// 1        7  //
+	/////////////////
+
+	// 插入数据
+	list := []int{5, 8, 3, 6, 4, 2, 7, 1}
+	node := Bst{}
+	for _, v := range list {
+		node.Add(v)
+	}
+	// 前序遍历
+	res1 := []int{5, 3, 2, 1, 4, 8, 6, 7}
+	// 中序遍历
+	res2 := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	// 后序遍历
+	res3 := []int{1, 2, 4, 3, 7, 6, 8, 5}
+
+	if !Same(node, func(b Bst) []int {
+		var list []int
+		b.PreOrder(func(node *Node) {
+			list = append(list, node.Num)
+		})
+		return list
+	}, res1) {
+		t.Error("前序遍历错误")
+	}
+
+	if !Same(node, func(b Bst) []int {
+		var list []int
+		b.PreOrder2(func(node *Node) {
+			list = append(list, node.Num)
+		})
+		return list
+	}, res1) {
+		t.Error("前序遍历错误2")
+	}
+
+	if !Same(node, func(b Bst) []int {
+		var list []int
+		b.InOrder(func(node *Node) {
+			list = append(list, node.Num)
+		})
+		return list
+	}, res2) {
+		t.Error("中序遍历错误")
+	}
+
+	if !Same(node, func(b Bst) []int {
+		var list []int
+		b.InOrder2(func(node *Node) {
+			list = append(list, node.Num)
+		})
+		//Pt(list)
+		return list
+	}, res2) {
+		t.Error("中序遍历错误2")
+	}
+
+	if !Same(node, func(b Bst) []int {
+		var list []int
+		b.PostOrder(func(node *Node) {
+			list = append(list, node.Num)
+		})
+		return list
+	}, res3) {
+		t.Error("后序遍历错误")
+	}
+
+	if !Same(node, func(b Bst) []int {
+		var list []int
+		b.PostOrder2(func(node *Node) {
+			list = append(list, node.Num)
+		})
+		//Pt(list)
+		return list
+	}, res3) {
+		t.Error("后序遍历错误2")
+	}
+}
